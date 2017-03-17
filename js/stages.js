3da40551bd98db58c668ea4e77f5e73f7faf7da6
Erica Nafziger <garbererica@gmail.com>
2017-03-16 17:09:55 -0700

    create smooth scrolling function that grabs number of stage clicked on

--- js/stages.js
+++ js/stages.js
@@ -1,17 +1,49 @@
 $(document).ready(function() {
 
   /* Check the location of each stage */
-  var stage1Top = $('#stage-1').offset().top;
-  var stage2Top = $('#stage-2').offset().top;
-  var stage3Top = $('#stage-3').offset().top;
-  var stage4Top = $('#stage-4').offset().top;
-  var stage5Top = $('#stage-5').offset().top;
-  var stage6Top = $('#stage-6').offset().top;
+  var stage1Top = $('#stage1').offset().top;
+  var stage2Top = $('#stage2').offset().top;
+  var stage3Top = $('#stage3').offset().top;
+  var stage4Top = $('#stage4').offset().top;
+  var stage5Top = $('#stage5').offset().top;
+  var stage6Top = $('#stage6').offset().top;
   $(window).scroll( function(){  /* Every time the window is scrolled ... */
 
     /* Check the current top of the window */
     var windowTop = $(window).scrollTop() + 250;
+    console.log(windowTop, stage1Top, stage2Top, stage3Top);
 
+    if (windowTop >= stage1Top && windowTop < stage2Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage1-nav').addClass('active-stage');
+    } else if (windowTop >= stage2Top && windowTop < stage3Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage2-nav').addClass('active-stage');
+    } else if (windowTop >= stage3Top && windowTop < stage4Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage3-nav').addClass('active-stage');
+    } else if (windowTop >= stage4Top && windowTop < stage5Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage4-nav').addClass('active-stage');
+    } else if (windowTop >= stage5Top && windowTop < stage6Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage5-nav').addClass('active-stage');
+    } else if (windowTop >= stage6Top) {
+      $('[id^=stage]').removeClass('active-stage');
+      $('#stage6-nav').addClass('active-stage');
+    }
+
+  });
+
+  //smooth scrolls to different stages on page
+
+  $("[id$=-nav]").click(function() { /* class or id of button/link that will be clicked */
+    var number = this.id.replace(/\D+/g, '');
+    console.log(number);
+      $('html, body').animate({
+          scrollTop: $("#stage" + number).offset().top - 50, /* class or id of div that will be scrolled to */
+          behavior: 'smooth',
+      }, 1000);
   });
 
 });
